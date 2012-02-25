5ded265571bc0aebe00767faf8af398d8ba39e58
Steve Dougherty <steve@asksteved.com>
2012-02-25 18:05:59 -0500

    Add types for integer arguments; tweak description.

--- probe.py
+++ probe.py
@@ -7,19 +7,19 @@ rand = random.SystemRandom()
 location = re.compile(r"Completed probe request: 0.\d+ -> (0.\d+)")
 
 #Set up argument parsing
-parser = argparse.ArgumentParser(description="Collect information gathered by making probes to random network locations, then update analysis thereof, generate graphs, and optionally upload the results.")
+parser = argparse.ArgumentParser(description="Make probes to random network locations, analyze the results for estimates of network size, generate graphs, and optionally upload the results.")
 parser.add_argument('-u', dest="upload", default=False,\
                     action="store_true", help="Upload updated analysis. This is not done by default.")
-parser.add_argument('-t', dest="numThreads", default=5,\
+parser.add_argument('-t', dest="numThreads", default=5, type=int,\
                     help="Number of simultanious probe threads to run. Default 5 threads.")
 parser.add_argument('--host', dest="host", default="127.0.0.1",\
                     help="Telnet host; Freenet node to connect to. Default 127.0.0.1.")
-parser.add_argument('-p', dest="port", default=2323,\
+parser.add_argument('-p', dest="port", default=2323, type=int,\
                     help="Port the target node is running TMCI on. Default port 2323.")
 parser.add_argument('-N', dest="numProbes", default=120, type=int,\
                     help="Number of total probes to make in each thread. Default 120 probes.")
-parser.add_argument('-w', dest="probeWait", default=30,
-                    help="Number of seconds to wait for a probe response. Default 10 seconds.")
+parser.add_argument('-w', dest="probeWait", default=30, type=int,\
+                    help="Number of seconds to wait for a probe response. Default 30 seconds.")
 
 args = parser.parse_args()
 
