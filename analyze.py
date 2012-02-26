1f53b92b4079f0e41405567c04837bb537c66648
Steve Dougherty <steve@asksteved.com>
2012-02-26 13:30:15 -0500

    Move analysis and upload switch into analyze.

---
+++ analyze.py
@@ -0,0 +1,57 @@
+import argparse
+import sqlite3
+import datetime
+import re
+#import math
+import subprocess
+
+parser = argparse.ArgumentParser(description="Analyze probe results for estimates of network size, generate graphs with gnuplot, and optionally upload the results.")
+parser.add_argument('-u', dest="upload", default=False,\
+                    action="store_true", help="Upload updated analysis. This is not done by default.")
+parser.add_argument('-d', dest="databaseFile", default="database.sql",\
+                    help="Path to database file. Default \"database.sql\"")
+parser.add_argument('-s', dest="startTime", default=1258866000, type=float,\
+                    help="Unix time for graph hour 0. Default 1258866000: 5 AM November 22, 2009")
+parser.add_argument('-f', dest="fullData", default="full_data",\
+                    help="Path to file to save analysis to. Default \"full_data\"")
+
+args = parser.parse_args()
+
+db = sqlite3.connect(args.databaseFile)
+
+#TODO: Are comments needed? This seems pretty self-explanatory. Is it of concern that
+#the 'now' used by sqlite will drift slightly between subsequent lines?
+
+timeSpans = [[0, "-1 hours"], [0, "-1 days"], [0, "-5 days"], [0, "-7 days"], [0, "-15 days"]]
+for span in timeSpans:
+	#TODO: When to fetch only distinct?
+	#string concatination because sqlite will not substitute paramters in strings.
+	#span[0] = db.execute("select distinct uid from uids where time > datetime('now','-'+?+' days')", [span[1]])
+	#TODO: SQL count()?
+	span[0] = len(db.execute("select distinct uid from uids where time > datetime('now',?)", [span[1]]).fetchall())
+	#TODO: Output hours line to all_data. Should also do summary?
+	#$HOURS $PEERS $PEERS5 $PEERS24 $PEERS24_1 $PEERS34 $PEERS34_1 $PEERS72 $PEERS72_1 $SIZE_AVG_24
+	#http://piratepad.net/ucUdssLhyE
+	#http://www.sqlite.org/lang.html
+	#print("Day -", span[1], ":", len(span[0].fetchall()))
+
+db.close()
+
+delta = datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(args.startTime)
+hour = long(delta.days * 24 + delta.seconds/3.6e3 + delta.microseconds/3.6e9)
+
+#Hour since stats epoch, peers in: last hour, last day, last five days, last 7 days, last 15 days.
+data = "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(hour, timeSpans[0][0], timeSpans[1][0], timeSpans[1][0], timeSpans[2][0], timeSpans[2][0], timeSpans[3][0], timeSpans[3][0], timeSpans[4][0], timeSpans[4][0], timeSpans[2][0]/5)
+print(data)
+
+#If the file doesn't exist, create it.
+dataFile = open(args.fullData, 'a')
+dataFile.close()
+
+with open(args.fullData, 'r+') as dataFile:
+#If there's already an entry for this hour, replace it. If not, append it.
+	if (re.search(r"^{0}[\d ]*$".format(hour), dataFile.read())):
+		subprocess.call([r'sed -E "s/^{0}[\d ]*$/{1}/"'.format(hour, data)])
+	else:
+		#TODO: Should be at end because the entire file was read?
+		dataFile.write(data)
--- probe.py
+++ probe.py
@@ -17,9 +17,7 @@ closestGreater = re.compile(r"Completed probe request: 0\.\d+ -> (0\.\d+)")
 #group 5: comma-separated peer UIDs
 parseTrace = re.compile(r"location=(0\.\d+)node UID=([-\d]*) prev UID=([-\d]*) peer locs=\[([-\d ,.]*)\] peer UIDs=\[([-\d ,]*)\]")
 
-parser = argparse.ArgumentParser(description="Make probes to random network locations, analyze the results for estimates of network size, generate graphs, and optionally upload the results.")
-parser.add_argument('-u', dest="upload", default=False,\
-                    action="store_true", help="Upload updated analysis. This is not done by default.")
+parser = argparse.ArgumentParser(description="Make probes to random network locations, saving the results to the specified database.")
 parser.add_argument('-t', dest="numThreads", default=5, type=int,\
                     help="Number of simultanious probe threads to run. Default 5 threads.")
 parser.add_argument('--host', dest="host", default="127.0.0.1",\
@@ -30,7 +28,7 @@ parser.add_argument('-N', dest="numProbes", default=120, type=int,\
                     help="Number of total probes to make in each thread. Default 120 probes.")
 parser.add_argument('-w', dest="probeWait", default=30, type=int,\
                     help="Number of seconds to wait for a probe response. Default 30 seconds.")
-parser.add_argument('-f', dest="databaseFile", default="database.sql",\
+parser.add_argument('-d', dest="databaseFile", default="database.sql",\
                     help="Path to database file. Default \"database.sql\"")
 
 args = parser.parse_args()
