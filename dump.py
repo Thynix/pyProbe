a311bdc0e4264f4a4b25ef119b90bbeacc1c98d1
Steve Dougherty <steve@asksteved.com>
2012-02-26 20:53:01 -0500

    Database dumping tool: number of known nodes, along with peers seen with them.

---
+++ dump.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+db = sqlite3.connect("database.sql")
+rows = db.execute("select distinct uid from uids").fetchall()
+
+print("{0} known UIDs.".format(len(rows)))
+#print(rows)
+
+for uid in rows:
+	probes = db.execute("select distinct probeID from traces where uid == ?", uid).fetchall()
+	peerUIDs = db.execute("select distinct peerUID from traces where uid == ?", uid).fetchall()
+	if len(probes) > 0:
+		print("Node UID {0} has been a node along the path of {1} probes, over which it has been seen with {2} distinct peers.".format(uid[0], len(probes), len(peerUIDs)))
+	else:
+		print("Node UID {0} has only been seen indirectly.".format(uid))
