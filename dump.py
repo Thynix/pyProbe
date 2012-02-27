b04aeae532466fbb2f92a62b24380f8366fbcb1a
Steve Dougherty <steve@asksteved.com>
2012-02-27 00:19:32 -0500

    List traces as well, offer choice between vacuum and histogram generation.

--- dump.py
+++ dump.py
@@ -1,15 +1,43 @@
 import sqlite3
+from array import array
+from sys import exit
 
 db = sqlite3.connect("database.sql")
-rows = db.execute("select distinct uid from uids").fetchall()
 
-print("{0} known UIDs.".format(len(rows)))
-#print(rows)
+print("Database contains:")
+print("* {0} distinct UIDs".format(db.execute("select count(distinct uid) from uids").fetchone()[0]))
+print("* {0} probes".format(db.execute("select count(probeID) from probes").fetchone()[0]))
+print("* {0} traces".format(db.execute("select count(traceNum) from traces").fetchone()[0]))
+#print("{0} new nodes.".format(db.execute("select min(time) as mintime from uids group by uid").fetchall()))
 
-for uid in rows:
-	probes = db.execute("select distinct probeID from traces where uid == ?", uid).fetchall()
-	peerUIDs = db.execute("select distinct peerUID from traces where uid == ?", uid).fetchall()
-	if len(probes) > 0:
-		print("UID {0} has been a node along {1} probes, over which it has been seen with {2} distinct peers.".format(uid[0], len(probes), len(peerUIDs)))
+uidblah = str(raw_input("h: calculate histogram of number of peers\nv: vaccuum database (requires no open transactions or active SQL statements)"))
+
+if uidblah == 'v':
+	print("Vacuuming")
+	db.execute("vacuum")
+	#TODO: Any way to have progress bar?
+	print("Complete")
+	exit(0)
+elif uidblah != 'h':
+	print("No option recognized; exiting")
+	exit(1)
+
+histogramMax = 100
+histogram = array('I')
+for _ in range(histogramMax):
+	histogram.append(0)
+
+for uid in db.execute("select distinct uid from uids").fetchall():
+	probes = db.execute("select count(distinct probeID) from traces where uid == ?", uid).fetchone()[0]
+	peerUIDs = db.execute("select count(traceNum) from traces where uid == ?", uid).fetchone()[0]
+	if probes > 0:
+		avgPeers = peerUIDs/probes
+		if avgPeers >= histogramMax:
+			histogram[histogramMax - 1] += 1
+		else:
+			histogram[avgPeers] += 1
+		#print("UID {0} has been a node along {1} probes, over which it has been seen with {2} distinct peers.".format(uid[0], len(probes), len(peerUIDs)))
 	#else:
 	#	print("UID {0} has only been seen indirectly.".format(uid[0]))
+
+print(histogram)
