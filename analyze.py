b0464b55047c8bffab4a272cac29f64170129cb8
Steve Dougherty <steve@asksteved.com>
2012-02-28 03:08:24 -0500

    Add claimed number of peer histograms to analyze.py. Hopefully faster (more idiomatic/sane) array initialization and omitting informational query on how many distinct UIDs probes have gone through.

--- analyze.py
+++ analyze.py
@@ -2,19 +2,23 @@ import argparse
 import sqlite3
 import datetime
 import re
-import subprocess
+from subprocess import call
+from array import array
 
-parser = argparse.ArgumentParser(description="Analyze probe results for estimates of network size, generate graphs with gnuplot, and optionally upload the results.")
+parser = argparse.ArgumentParser(description="Analyze probe results for estimates of network size and interconnectedness, generate graphs with gnuplot, and optionally upload the results.")
 parser.add_argument('-u', dest="upload", default=False,\
                     action="store_true", help="Upload updated analysis. This is not done by default.")
 parser.add_argument('-d', dest="databaseFile", default="database.sql",\
                     help="Path to database file. Default \"database.sql\"")
+#TODO: graph hour zero should be first probe time; probably should get rid of this.
 parser.add_argument('-s', dest="startTime", default=1258866000, type=float,\
                     help="Unix time for graph hour 0. Default 1258866000: 5 AM November 22, 2009")
 parser.add_argument('-f', dest="fullData", default="full_data",\
                     help="Path to file to save analysis to. Default \"full_data\"")
 parser.add_argument('-T', dest="recentSeconds", default=604800, type=long,\
                     help="A node is considered new if it was first seen after this many seconds in the past. A node is considered former if it was last seen before this many seconds in the past. Default 604800: one week.")
+parser.add_argument('--histogram-max', dest="histogramMax", default=100, type=int,\
+                    help="Maximum number of peers to consider for histogram generation; anything more than that is lumped into the highest category. Default 100.")
 
 args = parser.parse_args()
 
@@ -38,8 +42,6 @@ former = db.execute("select count(lastSeen) from (select max(time) as lastSeen f
 
 oneTime = db.execute("select count(appearances) from (select count(uid) as appearances from uids group by uid) where appearances == 1").fetchone()[0]
 
-db.close()
-
 delta = datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(args.startTime)
 hour = long(delta.days * 24 + delta.seconds/3.6e3 + delta.microseconds/3.6e9)
 
@@ -52,9 +54,40 @@ dataFile = open(args.fullData, 'a')
 dataFile.close()
 
 #Remove existing entry(/ies) for this hour and append updated one.
-subprocess.call(['sed','-i', r'/^{0}.*$/d'.format(hour), args.fullData])
+call(['sed','-i', r'/^{0}.*$/d'.format(hour), args.fullData])
 
 with open(args.fullData, 'a') as dataFile:
 	dataFile.write("{0}\n".format(data))
 
-subprocess.call(['gnuplot','plot.gnu'])
+call(['gnuplot','plot.gnu'])
+
+print("Initializing histogram")
+#Maximum: size one greater to account for zero.
+histogram = array('I', (0,)*(args.histogramMax + 1))
+
+print("Querying database for traces through distinct UIDs.")
+probeStats = db.execute("select count(traceNum), count(distinct probeID) from traces group by uid").fetchall()
+print("Analyzing results.")
+
+for traceAggregate in probeStats:
+        peerUIDs = traceAggregate[0]
+        probes = traceAggregate[1]
+        if probes > 0:
+                avgPeers = peerUIDs/probes
+                if avgPeers >= args.histogramMax:
+                        histogram[args.histogramMax] += 1
+                else:
+                        histogram[avgPeers] += 1
+
+print("Analysis complete, writing results.")
+with open("peerDist.dat", 'w') as output:
+        numberOfPeers = 0
+        for nodeCount in histogram:
+                output.write("{0} {1}\n".format(numberOfPeers, nodeCount))
+                numberOfPeers += 1
+
+print("Graphing.")
+call(["gnuplot","peer_dist.gnu"])
+
+print("Closing database.")
+db.close()
