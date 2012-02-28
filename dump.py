bffb5d7d12a6f562c1001646d79f0c264e192fa4
Steve Dougherty <steve@asksteved.com>
2012-02-28 00:38:43 -0500

    Progress bar and graph output of histogram.

--- dump.py
+++ dump.py
@@ -1,12 +1,15 @@
 import sqlite3
 from array import array
 from sys import exit
+from ProgressBar import ProgressBar
+from subprocess import call
 
 print("Connecting to database.")
 db = sqlite3.connect("database.sql")
 
 print("Database contains:")
-print("* {0} distinct UIDs".format(db.execute("select count(distinct uid) from uids").fetchone()[0]))
+numUIDs = db.execute("select count(distinct uid) from uids").fetchone()[0]
+print("* {0} distinct UIDs".format(numUIDs))
 print("* {0} probes".format(db.execute("select count(probeID) from probes").fetchone()[0]))
 print("* {0} traces".format(db.execute("select count(traceNum) from traces").fetchone()[0]))
 
@@ -36,7 +39,11 @@ histogram = array('I')
 for _ in range(histogramMax):
 	histogram.append(0)
 
+print("Processing {0} unique UIDs.".format(numUIDs))
+bar = ProgressBar(0, numUIDs, format='fixed')
+
 for uid in db.execute("select distinct uid from uids").fetchall():
+	bar.print_changed()
 	probes = db.execute("select count(distinct probeID) from traces where uid == ?", uid).fetchone()[0]
 	peerUIDs = db.execute("select count(traceNum) from traces where uid == ?", uid).fetchone()[0]
 	if probes > 0:
@@ -45,8 +52,11 @@ for uid in db.execute("select distinct uid from uids").fetchall():
 			histogram[histogramMax - 1] += 1
 		else:
 			histogram[avgPeers] += 1
-		#print("UID {0} has been a node along {1} probes, over which it has been seen with {2} distinct peers.".format(uid[0], len(probes), len(peerUIDs)))
-	#else:
-	#	print("UID {0} has only been seen indirectly.".format(uid[0]))
+	bar.increment_amount()
 
-print(histogram)
+with open("peerDist.dat", 'w') as output:
+	numberOfPeers = 0
+	for nodeCount in histogram:
+		output.write("{0} {1}\n".format(numberOfPeers, nodeCount))
+
+call(["gnuplot","peer_dist.gnu"])
---
+++ peer_dist.gnu
@@ -0,0 +1,16 @@
+set terminal png size 1200,800
+set output "peer_dist.png"
+
+#How to handle xrange? Could mean missing huge changes.
+set xrange [1:50]
+set xtics 5
+
+set xlabel "Reported Peers"
+set ylabel "Nodes"
+
+set style data histogram
+set style fill solid border -1
+
+set key off
+
+plot 'peerDist.dat' with boxes
