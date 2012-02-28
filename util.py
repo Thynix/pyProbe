6ae0575f08b8ce6009a21e5e993da7147abb5fb2
Steve Dougherty <steve@asksteved.com>
2012-02-28 03:51:17 -0500

    Add analysis option.

--- util.py
+++ util.py
@@ -9,9 +9,15 @@ with sqlite3.connect("database.sql") as db:
 	print("* {0} probes".format(db.execute("select count(probeID) from probes").fetchone()[0]))
 	print("* {0} traces".format(db.execute("select count(traceNum) from traces").fetchone()[0]))
 
-	if str(raw_input("Enter 'v' to vaccuum (requires no open transactions or active SQL statements); anything else to exit: ")) == 'v':
+	choice = str(raw_input("Enter\n * v to vaccuum (requires no open transactions or active SQL statements)\n * a to analyze\n * anything else to exit.\n> "))
+	
+	if choice == 'v':
 		print("Vacuuming...")
 		db.execute("vacuum")
 		print("Vacuuming complete. Committing...")
 		db.commit()
-		print("Done.")	
+		print("Done.") 
+	elif choice == 'a':
+		print("Analyzing...")
+		db.execute("analyze")
+		db.commit()
