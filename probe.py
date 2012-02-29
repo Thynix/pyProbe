73427e754b3046bc1dd37b13676cfa62b335490b
Steve Dougherty <steve@asksteved.com>
2012-02-29 16:42:57 -0500

    Concurrency implementation which crashes but is event-based.

--- probe.py
+++ probe.py
@@ -8,8 +8,14 @@ import time
 import shlex
 from sys import executable, exit
 from subprocess import Popen
-#TODO: function to print line appended to current timestamp
+import concurrent.futures
+#TODO: function to log line appended to current timestamp. Logger should be able to do that.
+
+#Telnet prompt
+prompt="TMCI> "
+
 rand = random.SystemRandom()
+
 #Not much use; stored anyway.
 closestGreater = re.compile(r"Completed probe request: 0\.\d+ -> (0\.\d+)")
 
@@ -20,6 +26,71 @@ closestGreater = re.compile(r"Completed probe request: 0\.\d+ -> (0\.\d+)")
 #group 3: comma-separated peer locations
 #group 4: comma-separated peer UIDs
 parseTrace = re.compile(r"location=(0\.\d+)node UID=([-\d]*) prev UID=[-\d]* peer locs=\[([-\d ,.]*)\] peer UIDs=\[([-\d ,]*)\]")
+
+class traceResult:
+	def __init__(self, location, UID, peerLocs, peerUIDs):
+		self.location = location
+		self.UID = UID
+		self.peerLocs = peerLocs
+		self.peerUIDs = peerUIDs
+
+class probeResult:
+	def __init__(self, target, start=datetime.datetime.utcnow()):
+		self.target = target
+		#Time probe completed
+		self.start = start
+		
+		self.closest = 0.0
+		self.traces = []
+		self.end = datetime.datetime.utcnow()
+
+def probe(args, wait = 0):
+	if wait > 0:
+		time.sleep(wait)
+	
+	target = rand.random()	
+
+	tn = telnetlib.Telnet(args.host, args.port)
+	
+	#Read through intial help message.
+	tn.read_until(prompt)
+	
+	if args.verbosity > 0:
+		print("{0}: Starting probe to {1}.".format(datetime.datetime.now(), target))
+	
+	tn.write("PROBE: {0}\n".format(target))
+	
+	result = probeResult(target)
+	raw = tn.read_until(prompt, args.probeTimeout)
+	
+	#TODO: What if timeout elapses? Need to skip parsing attempt.
+	if args.verbosity > 0:
+		print("{0}: Probe finished. Took {1} sec.".format(datetime.datetime.now(), (datetime.datetime.utcnow() - result.start).seconds))
+
+	if args.verbosity > 1:
+		#TODO: Reasonable to start and end block with newlines? Might be misleading for the end.
+		print("---Begin raw response---\n{0}\n---End raw response---".format(raw))
+	
+	#Check for closest location to target location reached. If no such entry exists, insert NULL/None.
+	closest = closestGreater.search(raw)
+	if closest is not None:
+		closest = closest.group(1)
+	
+	result.closest = closest
+	
+	#Parse for locations and UIDs of each trace's node and its peers.
+	for trace in parseTrace.findall(raw):
+		#Of node described by current trace.
+		location = trace[0]
+		UID = trace[1]
+		#Remove whitespace so that numerically identical values aren't considered different.
+		peerLocs = filter(None, trace[2].split(','))
+		peerUIDs = filter(None, trace[3].split(','))
+		
+		result.traces += [traceResult(location, UID, peerLocs, peerUIDs)]
+	
+	result.end = datetime.datetime.utcnow()
+	return result
 
 parser = argparse.ArgumentParser(description="Make probes to random network locations, saving the results to the specified database.")
 
@@ -41,35 +112,8 @@ parser.add_argument('-v', dest="verbosity", action='count',\
 
 args = parser.parse_args()
 
-#Uses thread option to spawn more copies of this script to total the requested number of threads.
-#TODO: Is there a more sane way to handle this? Are forks something different than this?
-if args.numThreads > 1:
-	argString = executable+" "+__file__+" "
-	argString += "-t 1 --host {0} -p {1} --timeout {2} --wait {3} -d {4}".format(args.host, args.port, args.probeTimeout, args.probeWait, args.databaseFile)
-	if args.verbosity > 0:
-		argString += "-"
-		argString += "v"*args.verbosity
-	
-	subprocessArguments = shlex.split(argString)
-	
-	#TODO: Take output from subprocesses (if verbose). Is there any way to do this without waiting until the threads terminate? May have to log to a file then.
-	threads = [Popen(subprocessArguments)] * args.numThreads
-	#TODO: Cleaner way to just wait until sigterm? Is it required to pass this on to subprocesses, or does the OS do that?
-	#TODO: Once the probe is a class, the signal handler should roll back changes.
-		#TODO: Arg, hang on: "When threads are enabled, this function can only be called from the main thread; attempting to call it from other threads will cause a ValueError exception to be raised."
-	for thread in threads:
-		thread.communicate()
-	
-elif args.numThreads < 1:
-	print("Cannot run fewer than one thread.")
-	exit(1)
-
-#TODO: Consider moving things not needed to spawn subprocesses below this point, as well as moving this stuff into a class to make for easier signal handling.
-
+#Ensure the database holds the required tables, columns, and indicies. Better now than during each thread.
 db = sqlite3.connect(args.databaseFile)
-#Cursor needed for lastrowid so that traces can be inserted under the correct ProbeID.
-cursor = db.cursor()
-
 db.execute("create table if not exists uids(uid, time)")
 #Index to speed up time-based UID analysis.
 db.execute("create index if not exists uid_index on uids(uid)")
@@ -80,77 +124,62 @@ db.execute("create table if not exists probes(probeID INTEGER PRIMARY KEY, time,
 
 #traceID is not unique among traces for a given probe; only one peer location or UID is stored per entry.
 db.execute("create table if not exists traces(probeID, traceNum, uid, location, peerLoc, peerUID)")
-#Index to speed up histogram generation. TODO: May want to remove any indicies that end up being misguided.
+#Index to speed up histogram generation. TODO: Remove any indicies which end up being misguided.
 db.execute("create index if not exists probeID_index on traces(traceNum, probeID)")
 db.execute("create index if not exists UID_index on traces(uid)")
 
-prompt="TMCI> "
-tn = telnetlib.Telnet(args.host, args.port)
+db.commit()
 
-#Read through intial help message.
-tn.read_until(prompt)
+if args.numThreads < 1:
+	print("Cannot run fewer than one thread.")
+	exit(1)
 
-while True:
-	target = rand.random()
-	
-	if args.verbosity > 0:
-		print("{0}: Starting probe to {1}.".format(datetime.datetime.now(), target))
-	
-	tn.write("PROBE: {0}\n".format(target))
-	
-	beforeProbe = datetime.datetime.utcnow()
-	raw = tn.read_until(prompt, args.probeTimeout)
-	afterProbe = datetime.datetime.utcnow()
-	
-	#TODO: What if timeout elapses? Need to skip parsing attempt.
-	if args.verbosity > 0:
-		print("{0}: Probe finished. Took {1} sec. Saving traces.".format(datetime.datetime.now(), (afterProbe - beforeProbe).seconds))
+#Cursor needed for lastrowid so that traces can be inserted under the correct ProbeID.
+cursor = db.cursor()
 
-	if args.verbosity > 1:
-		#TODO: Reasonable to start and end block with newlines? Might be misleading for the end.
-		print("---Begin raw response---\n{0}\n---End raw response---".format(raw))
-	
-	#Check for closest location to target location reached. If no such entry exists, insert NULL/None.
-	closest = None
-	location = closestGreater.search(raw)
-	if location is not None:
-		closest = location.group(1)
-	
-	#NULL prompt the database to assign a key as probeID is an INTEGER PRIMARY KEY.
-	cursor.execute("insert into probes(probeID, time, target, closest) values (NULL, ?, ?, ?)", [afterProbe, target, closest])
-	probeID = cursor.lastrowid
-	
-	#Parse for locations and UIDs of each trace's node and its peers.
-	traceID = 0
-	for trace in parseTrace.findall(raw):
-		#Of node described by current trace.
-		location = trace[0]
-		UID = trace[1]
-		#Remove whitespace so that numerically identical values aren't considered different.
-		peerLocs = filter(None, trace[2].split(','))
-		peerUIDs = filter(None, trace[3].split(','))
 
-		for uid in peerUIDs + [UID]:
-			db.execute("insert into uids(uid, time) values (?, ?)", (uid, afterProbe))
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=args.numThreads)
+#TODO: Duplication between this and the refilling line.
+threads = []
+#Stagger starting time throughout wait period.
+staggerTime = args.probeWait / args.numThreads
+for i in range(args.numThreads):
+	threads += [executor.submit(probe, args, i*staggerTime)]
+
+#Keep numThreads probe threads running until terminated. Must refill container.
+for thread in concurrent.futures.as_completed(threads):
+	if thread.exception() is not None:
+		#TODO: logger
+		print("Probe thread exited with exception: {0}".format(thread.exception()))
+	else:
+		result = thread.result()
 		
-		assert len(peerLocs) == len(peerUIDs)
-		for i in range(len(peerLocs)):
-			db.execute("insert into traces(probeID, traceNum, uid, location, peerLoc, peerUID) values (?, ?, ?, ?, ?, ?)", (probeID, traceID, UID, location, peerLocs[i], peerUIDs[i]))
-		traceID += 1
-	
-	#Commit after inserting each probe and before waiting.
-	db.commit()
-	
-	#If the minimum wait time between probes has not elapsed, wait that long.
-	#TODO: http://twistedmatrix.com/documents/current/api/twisted.internet.task.LoopingCall.html
-	afterDatabase = datetime.datetime.utcnow()
-	databaseTime = (afterDatabase - afterProbe).seconds
-	sinceProbe = (afterDatabase - beforeProbe).seconds
-	wait = args.probeWait - sinceProbe
-	if args.verbosity > 0:
-		print("{0}: {1} traces committed to database in {2} seconds. {3} sec since probe. Waiting {4} sec.".format(datetime.datetime.now(), traceID, databaseTime, sinceProbe, wait))
-	if wait > 0:
-		time.sleep(wait)
+		#NULL prompt the database to assign a key as probeID is an INTEGER PRIMARY KEY.
+		cursor.execute("insert into probes(probeID, time, target, closest) values (NULL, ?, ?, ?)", [result.end, result.target, result.closest])
+		
+		probeID = cursor.lastrowid
+		
+		traceID = 0
+		for trace in result.traces:
+			for uid in trace.peerUIDs + [trace.UID]:
+				db.execute("insert into uids(uid, time) values (?, ?)", (uid, result.end))
 
-cursor.close()
-db.close()
+			#TODO: Cleaner way to loop over two containers of the same length simultaniously?
+			assert len(trace.peerLocs) == len(trace.peerUIDs)
+			for i in range(len(trace.peerLocs)):
+				db.execute("insert into traces(probeID, traceNum, uid, location, peerLoc, peerUID) values (?, ?, ?, ?, ?, ?)", (probeID, traceID, trace.UID, trace.location, trace.peerLocs[i], trace.peerUIDs[i]))	
+			traceID += 1
+	
+		db.commit()
+
+		#If the minimum wait time between probes has not elapsed, wait that long.
+		#TODO: http://twistedmatrix.com/documents/current/api/twisted.internet.task.LoopingCall.html
+		afterDatabase = datetime.datetime.utcnow()
+		databaseTime = (afterDatabase - result.end).seconds
+		sinceProbe = (afterDatabase - result.start).seconds
+		wait = args.probeWait - sinceProbe
+		if args.verbosity > 0:
+			print("{0}: {1} traces committed to database in {2} seconds. {3} sec since probe. Waiting {4} sec.".format(datetime.datetime.now(), traceID, databaseTime, sinceProbe, wait))
+
+		#Replace completed thread. TODO: Does this actually work?
+		threads += [executor.submit(probe, args, wait)]
