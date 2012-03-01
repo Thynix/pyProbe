ef33a074064231609c392111c13507a7944e5c90
Steve Dougherty <steve@asksteved.com>
2012-03-01 14:32:11 -0500

    With a combination of Twisted fromThread calls and daemon threads, initial implementation.

--- probe.py
+++ probe.py
@@ -6,10 +6,12 @@ import sqlite3
 import datetime
 import time
 import shlex
-from sys import executable, exit
-from subprocess import Popen
-import concurrent.futures
+from sys import exit
+from twisted.internet import reactor
+from signal import signal, SIGINT, SIG_DFL
+from threading import Thread
 #TODO: function to log line appended to current timestamp. Logger should be able to do that.
+#TODO: Move stuff that doesn't need to be global into a main()
 
 #Telnet prompt
 prompt="TMCI> "
@@ -27,6 +29,33 @@ closestGreater = re.compile(r"Completed probe request: 0\.\d+ -> (0\.\d+)")
 #group 4: comma-separated peer UIDs
 parseTrace = re.compile(r"location=(-?\d+\.\d+)node UID=(-?\d+) prev UID=-?\d+ peer locs=\[([-\d ,.]*)\] peer UIDs=\[([-\d ,]*)\]")
 
+def insert(args, result):
+	start = datetime.datetime.utcnow()
+	db = sqlite3.connect(args.databaseFile)
+	#Cursor needed for lastrowid so that traces can be inserted under the correct ProbeID.
+	cursor = db.cursor()
+	#NULL prompt the database to assign a key as probeID is an INTEGER PRIMARY KEY.
+	cursor.execute("insert into probes(probeID, time, target, closest) values (NULL, ?, ?, ?)", [result.end, result.target, result.closest])
+	
+	probeID = cursor.lastrowid
+	
+	traceID = 0
+	for trace in result.traces:
+		for uid in trace.peerUIDs + [trace.UID]:
+			db.execute("insert into uids(uid, time) values (?, ?)", (uid, result.end))
+
+		#TODO: Cleaner way to loop over two containers of the same length simultaniously?
+		assert len(trace.peerLocs) == len(trace.peerUIDs)
+		for i in range(len(trace.peerLocs)):
+			db.execute("insert into traces(probeID, traceNum, uid, location, peerLoc, peerUID) values (?, ?, ?, ?, ?, ?)", (probeID, traceID, trace.UID, trace.location, trace.peerLocs[i], trace.peerUIDs[i]))	
+		traceID += 1
+
+	db.commit()
+	cursor.close()
+	db.close()
+	end = datetime.datetime.utcnow()
+	print("{0}: Committed {1} traces in {2} seconds.".format(end, traceID, (end - start).seconds))
+
 class traceResult:
 	def __init__(self, location, UID, peerLocs, peerUIDs):
 		self.location = location
@@ -37,68 +66,73 @@ class traceResult:
 class probeResult:
 	def __init__(self, target, start=datetime.datetime.utcnow()):
 		self.target = target
-		#Time probe completed
 		self.start = start
 		
+		#Should be updated.
 		self.closest = 0.0
 		self.traces = []
+		#Time probe completed
 		self.end = datetime.datetime.utcnow()
 
 def probe(args, wait = 0):
-	if wait > 0:
-		time.sleep(wait)
-	
-	target = rand.random()	
-
-	tn = telnetlib.Telnet(args.host, args.port)
-	
-	#Read through intial help message.
-	tn.read_until(prompt)
-	
-	if args.verbosity > 0:
-		print("{0}: Starting probe to {1}.".format(datetime.datetime.now(), target))
-	
-	tn.write("PROBE: {0}\n".format(target))
-	
-	result = probeResult(target)
-	raw = tn.read_until(prompt, args.probeTimeout)
-	
-	#TODO: What if timeout elapses? Need to skip parsing attempt.
-	if args.verbosity > 0:
-		print("{0}: Probe finished. Took {1} sec.".format(datetime.datetime.now(), (datetime.datetime.utcnow() - result.start).seconds))
-
-	if args.verbosity > 1:
-		#TODO: Reasonable to start and end block with newlines? Might be misleading for the end.
-		print("---Begin raw response---\n{0}\n---End raw response---".format(raw))
-	
-	#Check for closest location to target location reached. If no such entry exists, insert NULL/None.
-	closest = closestGreater.search(raw)
-	if closest is not None:
-		closest = closest.group(1)
-	
-	result.closest = closest
-	
-	#Parse for locations and UIDs of each trace's node and its peers.
-	for trace in parseTrace.findall(raw):
-		#Of node described by current trace.
-		location = trace[0]
-		UID = trace[1]
-		#TODO: Ideally there'd be a way to find just the numbers with the regex,
-		#but that's been difficult.
-		peerLocs = []
-		for val in trace[2].split(','):
-			#Ignore empty string
-			if val: 
-				peerLocs.append(float(val))
-		peerUIDs = []
- 		for val in trace[3].split(','):
-			if val:
-				peerUIDs.append(long(val))
+	while True:
+		if wait > 0:
+			if args.verbosity > 0:
+				print("Waiting {0} seconds before starting probe.".format(wait))
+			time.sleep(wait)
 		
-		result.traces.append(traceResult(location, UID, peerLocs, peerUIDs))
-	
-	result.end = datetime.datetime.utcnow()
-	return result
+		target = rand.random()	
+
+		tn = telnetlib.Telnet(args.host, args.port)
+		
+		#Read through intial help message.
+		tn.read_until(prompt)
+		
+		if args.verbosity > 0:
+			print("{0}: Starting probe to {1}.".format(datetime.datetime.now(), target))
+		
+		tn.write("PROBE: {0}\n".format(target))
+		
+		result = probeResult(target)
+		raw = tn.read_until(prompt, args.probeTimeout)
+		
+		#TODO: What if timeout elapses? Need to skip parsing attempt.
+		if args.verbosity > 0:
+			print("{0}: Probe finished. Took {1} sec.".format(datetime.datetime.now(), (datetime.datetime.utcnow() - result.start).seconds))
+
+		if args.verbosity > 1:
+			#TODO: Reasonable to start and end block with newlines? Might be misleading for the end.
+			print("---Begin raw response---\n{0}\n---End raw response---".format(raw))
+		
+		#Check for closest location to target location reached. If no such entry exists, insert NULL/None.
+		closest = closestGreater.search(raw)
+		if closest is not None:
+			closest = closest.group(1)
+		
+		result.closest = closest
+		
+		#Parse for locations and UIDs of each trace's node and its peers.
+		for trace in parseTrace.findall(raw):
+			#Of node described by current trace.
+			location = trace[0]
+			UID = trace[1]
+			#TODO: Ideally there'd be a way to find just the numbers with the regex,
+			#but that's been difficult.
+			peerLocs = []
+			for val in trace[2].split(','):
+				#Ignore empty string
+				if val: 
+					peerLocs.append(float(val))
+			peerUIDs = []
+			for val in trace[3].split(','):
+				if val:
+					peerUIDs.append(long(val))
+			
+			result.traces.append(traceResult(location, UID, peerLocs, peerUIDs))
+		
+		result.end = datetime.datetime.utcnow()
+		reactor.callFromThread(insert, args, result)
+		wait = args.probeWait - (result.end - result.start).seconds
 
 parser = argparse.ArgumentParser(description="Make probes to random network locations, saving the results to the specified database.")
 
@@ -137,57 +171,30 @@ db.execute("create index if not exists probeID_index on traces(traceNum, probeID
 db.execute("create index if not exists UID_index on traces(uid)")
 
 db.commit()
+db.close()
 
 if args.numThreads < 1:
 	print("Cannot run fewer than one thread.")
 	exit(1)
 
-#Cursor needed for lastrowid so that traces can be inserted under the correct ProbeID.
-cursor = db.cursor()
+def shutDown(num, frame):
+	#Restore default handler
+	signal(SIGINT, SIG_DFL)
+	print("Shutting down. Hit ctrl-c again to exit.")
+	reactor.stop()
 
+def startThreads(threads):
+	for thread in threads:
+		thread.start()
 
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=args.numThreads)
-#TODO: Duplication between this and the refilling line.
-threads = []
 #Stagger starting time throughout wait period.
 staggerTime = args.probeWait / args.numThreads
+threads = []
 for i in range(args.numThreads):
-	threads += [executor.submit(probe, args, i*staggerTime)]
+	thread = Thread(target=probe, args=(args, i*staggerTime))
+	thread.daemon = True
+	threads.append(thread)
 
-#Keep numThreads probe threads running until terminated. Must refill container.
-for thread in concurrent.futures.as_completed(threads):
-	if thread.exception() is not None:
-		#TODO: logger
-		print("Probe thread exited with exception: {0}".format(thread.exception()))
-	else:
-		result = thread.result()
-		
-		#NULL prompt the database to assign a key as probeID is an INTEGER PRIMARY KEY.
-		cursor.execute("insert into probes(probeID, time, target, closest) values (NULL, ?, ?, ?)", [result.end, result.target, result.closest])
-		
-		probeID = cursor.lastrowid
-		
-		traceID = 0
-		for trace in result.traces:
-			for uid in trace.peerUIDs + [trace.UID]:
-				db.execute("insert into uids(uid, time) values (?, ?)", (uid, result.end))
-
-			#TODO: Cleaner way to loop over two containers of the same length simultaniously?
-			assert len(trace.peerLocs) == len(trace.peerUIDs)
-			for i in range(len(trace.peerLocs)):
-				db.execute("insert into traces(probeID, traceNum, uid, location, peerLoc, peerUID) values (?, ?, ?, ?, ?, ?)", (probeID, traceID, trace.UID, trace.location, trace.peerLocs[i], trace.peerUIDs[i]))	
-			traceID += 1
-	
-		db.commit()
-
-		#If the minimum wait time between probes has not elapsed, wait that long.
-		#TODO: http://twistedmatrix.com/documents/current/api/twisted.internet.task.LoopingCall.html
-		afterDatabase = datetime.datetime.utcnow()
-		databaseTime = (afterDatabase - result.end).seconds
-		sinceProbe = (afterDatabase - result.start).seconds
-		wait = args.probeWait - sinceProbe
-		if args.verbosity > 0:
-			print("{0}: {1} traces committed to database in {2} seconds. {3} sec since probe. Waiting {4} sec.".format(datetime.datetime.now(), traceID, databaseTime, sinceProbe, wait))
-
-		#Replace completed thread. TODO: Does this actually work?
-		threads += [executor.submit(probe, args, wait)]
+reactor.callWhenRunning(signal, SIGINT, shutDown)
+reactor.callWhenRunning(startThreads, threads)
+reactor.run()
