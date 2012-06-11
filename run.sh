650528688cdd059c86db4767a4fe931282f749a3
Steve Dougherty <steve@asksteved.com>
2012-06-11 17:11:09 -0400

    Check for pid file in run script.
    Gives better behavior in starting and stopping,

--- run.sh
+++ run.sh
@@ -1,19 +1,70 @@
 #!/bin/bash
+#Exit on error
+set -o errexit
 if [ "X`id -u`" = "X0" -a -z "$RUN_AS_USER" ]
 then
     echo "Do not run this script as root."
     exit 1
 fi
 
-pid=probe.pid
-log=probe.log
+pidFile="probe.pid"
+logFile="probe.log"
 
 start() {
-    twistd --python=probe.py --logfile="$log" --pidfile="$pid"
+    if [ -e "$pidFile" ]
+    then
+        if kill -0 `cat "$pidFile"`
+        then
+            echo "Probe is already running."
+            return
+        else
+            echo "Removing stale PID file."
+            rm "$pidFile"
+        fi
+    fi
+    echo "Starting probe."
+    twistd --python=probe.py --logfile="$logFile" --pidfile="$pidFile"
 }
 
 stop() {
-    kill `cat "$pid"`
+    if [ -e "$pidFile" ]
+    then
+        if kill -0 `cat "$pidFile"`
+        then
+            echo "Stopping probe."
+            kill -INT `cat "$pidFile"`
+            i=0
+            #twistd removes the pid file on shutdown.
+            while [ -e "$pidFile" ]
+            do
+                let "i += 1"
+                if [ $i -eq "100" ]
+                then
+                    echo "Waiting..."
+                    i=0
+                fi
+            done
+            return
+        fi
+    fi
+    echo "Probe is not running."
+}
+
+status() {
+    if [ -e "$pidFile" ]
+    then
+        if kill -0 `cat "$pidFile"`
+        then
+            echo "Probe is running."
+            return
+        fi
+    fi
+    #If the PID file does not exist or a process with that PID is not running.
+    echo "Probe is not running."
+}
+
+log() {
+    tail -F "$logFile"
 }
 
 case "$1" in
@@ -31,11 +82,20 @@ case "$1" in
         ;;
 
     'status')
-        tail -F "$log"
-    ;;
+        status
+        ;;
+
+    'console')
+        start
+        log
+        ;;
+
+    'log')
+        log
+        ;;
 
     *)
-        echo "Usage: $0 { start | stop | restart | status }"
+        echo "Usage: $0 { console | start | stop | restart | status | log }"
         exit 1
         ;;
 
