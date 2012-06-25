d5ae45e565dd7d1aad7e298bed00e6efd2acfe92
Steve Dougherty <steve@asksteved.com>
2012-06-25 13:44:42 -0400

    Add detailed error breakdown option.

--- util.py
+++ util.py
@@ -22,12 +22,41 @@ def DivSafe(num):
     else:
         return num
 
-choice = str(raw_input("Enter:\n * a to analyze\n * s to view statistics\n * v to vaccuum (requires no open transactions or active SQL statements)\n * anything else to exit.\n> "))
+choice = str(raw_input("Enter:\n * a to analyze\n * e to view per-type error breakdown\n * s to view overall statistics\n * v to vaccuum (requires no open transactions or active SQL statements)\n * anything else to exit\n> "))
 with sqlite3.connect(args.databaseFile) as db:
     if choice == 'a':
         print("Analyzing...")
         db.execute("analyze")
         db.commit()
+    elif choice == 'e':
+        probe_types = [ "BANDWIDTH", "BUILD", "IDENTIFIER", "LINK_LENGTHS", "LOCATION", "STORE_SIZE", "UPTIME_48H", "UPTIME_7D" ]
+        errors = {}
+        total_count = 0
+
+        class error_occurences:
+            def __init__(self, error_list):
+                self.error_list = error_list
+                self.count = 0
+                for error in self.error_list:
+                    self.count += error[1]
+
+            def __enter__(self):
+                pass
+
+            def __exit__(self, type, value, traceback):
+                pass
+
+        for probe_type in probe_types:
+            errors[probe_type] = error_occurences(db.execute("""select "error_type", count(*) from "error" where "probe_type" == '{0}' group by "error_type" """.format(probe_type)).fetchall())
+            total_count += errors[probe_type].count
+
+        print("Errors stored: {0:n} total".format(total_count))
+        for probe_type in probe_types:
+            error = errors[probe_type]
+            print(" * {0}: {1:n} ({2:.1f}%)".format(probe_type, error.count, error.count/DivSafe(total_count)*100))
+            for error_entry in error.error_list:
+                print(" *     {0}: {1:n} ({2:.1f}%)".format(error_entry[0], error_entry[1], error_entry[1]/DivSafe(error.count)*100))
+
     elif choice == 's':
         tables = [ "bandwidth", "build", "identifier", "link_lengths", "location", "store_size", "uptime_48h", "uptime_7d", "refused", "error" ]
         #Use single quotes for values; double quotes for identifiers.
@@ -37,6 +66,7 @@ with sqlite3.connect(args.databaseFile) as db:
         for table in tables:
             count = 0
             if table == "link_lengths":
+                #NOTE: Assumes time is an identifier, which may not be the case. This is only useful for
                 count = db.execute("""select count(*) from (select "time" from "link_lengths" group by "time")""").fetchone()[0]
             else:
                 count = db.execute("""select count(*) from "{0}" """.format(table)).fetchone()[0]
