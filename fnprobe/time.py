b8486f22d5c5505b9a29a31433ef27238e327b71
Steve Dougherty <steve@asksteved.com>
2012-12-22 09:57:00 -0500

    Move database timestamp conversion to module.

--- fnprobe/time.py
+++ fnprobe/time.py
@@ -1,4 +1,6 @@
 import calendar
+import datetime
+import exceptions
 
 def toPosix(dt):
 	return int(calendar.timegm(dt.utctimetuple()))
@@ -10,4 +12,15 @@ def totalSeconds(delta):
 	# Seconds per microsecond: 1/1000000
 	return delta.days * 86400 + delta.seconds + delta.microseconds / 1000000
 
+timestampFormat = u"%Y-%m-%d %H:%M:%S.%f"
+sqliteTimestampFormat = u"%Y-%m-%d %H:%M:%S"
+def timestamp(string):
+	"""
+	Parses a database-formatted timestamp into a datetime.
+	"""
+	# Timestamp may or may not specify milliseconds.
+	try:
+		return datetime.datetime.strptime(string, timestampFormat)
+	except exceptions.ValueError:
+		return datetime.datetime.strptime(string, sqliteTimestampFormat)
 
