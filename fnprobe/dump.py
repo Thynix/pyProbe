aad9440b99a18841c01063c898f4f1ecfc2ac79a
Steve Dougherty <steve@asksteved.com>
2013-06-18 00:44:55 -0400

    Dump to file as superuser.

--- fnprobe/dump.py
+++ fnprobe/dump.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
+from ConfigParser import SafeConfigParser
 import argparse
 import datetime
 from sys import exit, stderr
-
-import update_db
+import psycopg2
+import db
 
 today = datetime.date.today().strftime('%Y-%m-%d')
 
@@ -14,6 +15,14 @@ parser.add_argument('--up-to', dest='up_to', default=today,
                          ' ISO 8601: 2013-02-27 is February 27th, 2013.')
 parser.add_argument('--days', dest='days', type=int, default=7,
                     help='Number of days back to dump. Defaults to 7.')
+parser.add_argument('--suffix', dest='suffix', default='week',
+                    help='Suffix to add to the end of the filename. Defaults '
+                         'to week. An example filename is '
+                         '2013-02-27-bandwidth-week.sql')
+parser.add_argument('--output-dir', dest='out_dir',
+                    default='/var/lib/postgresql',
+                    help='Directory to dump into. Defaults to '
+                         '"/var/lib/postgresql"')
 args = parser.parse_args()
 
 
@@ -24,12 +33,25 @@ if not args.days > 0:
     print("Days must be positive. {0} is not.".format(args.days))
     exit(1)
 
-database = update_db.main()
+parser = SafeConfigParser()
+parser.read("database.config")
+config = parser.defaults()
 
-cur = database.read.cursor()
+# The database user is the one writing to the file, not this client,
+# Cannot meaningfully check for output directory access here. In order to
+# COPY TO a file, must connect as super user. (Attempting to COPY TO STDOUT
+# causes a segmentation fault,)
 
-for table in database.table_names:
-    print("Copying '{0}'.".format(table), file=stderr)
+cur = psycopg2.connect(database=config['database'], user='postgres').cursor()
+
+for table in db.list_tables(cur):
+    # link_lengths does not have a "time" column.
+    if table == 'link_lengths':
+        continue
+
+    filename = '{0}/{1}-{2}-{3}.sql'.format(args.out_dir, args.up_to, table,
+                                            args.suffix)
+    print("Copying '{0}' to '{1}'.".format(table, filename), file=stderr)
     cur.execute("""
         COPY
           (SELECT
@@ -38,4 +60,5 @@ for table in database.table_names:
             "{0}"
           WHERE
             "time" BETWEEN %(start)s AND %(end)s)
-        TO STDOUT""".format(table), {'start': start_date, 'end': up_to_date})
+        TO %(file)s""".format(table), {'start': start_date,
+                                       'end': up_to_date, 'file': filename})
