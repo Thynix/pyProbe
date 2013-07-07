81f2b0d5c3e41714d6493c2a882d483dce1ab941
Steve Dougherty <steve@asksteved.com>
2013-07-06 23:52:11 -0400

    Fix import; missing argument.

--- fnprobe/update_ids.py
+++ fnprobe/update_ids.py
@@ -1,10 +1,10 @@
-from fnprobe.db import list_tables, update_id_sequence
+from db import list_tables, update_id_sequence
 import update_db
 
 # No need to commit changes - sequence changes are not transactional.
 database = update_db.main()
 cur = database.maintenance.cursor()
 
-for table in list_tables():
+for table in list_tables(cur):
     print("Updating %s" % table)
     update_id_sequence(cur, table)
