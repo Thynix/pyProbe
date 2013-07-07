8217f3e8695d7c0ced8eef306aecee876217771f
Steve Dougherty <steve@asksteved.com>
2013-07-06 22:16:38 -0400

    Revert "Remove needless sequence update."
    This reverts commit f2f430471413786fbd0ee3ea5c88941d438a1bea.
    
    This wasn't a sequence update - it was permissions.

--- fnprobe/db.py
+++ fnprobe/db.py
@@ -154,6 +154,21 @@ class Database:
               "{1}"
             """.format(tables, auth['read_user']))
 
+            # Must be able to update sequences to insert using the default
+            # value of the next one from the sequence.
+            # TODO: More idiomatic way to append to each element?
+            sequences = ','.join(map(lambda name: '"' + name + '_id_seq"',
+                                     self.table_names))
+
+            cur.execute("""
+            GRANT
+              UPDATE
+            ON SEQUENCE
+              {0}
+            TO
+              "{1}"
+            """.format(sequences, auth['add_user']))
+
             self.maintenance.commit()
 
     def create_new(self):
