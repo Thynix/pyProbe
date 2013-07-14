e0896e73c9fa67318d522d0b33127c104c63cdee
Steve Dougherty <steve@asksteved.com>
2013-07-14 16:58:39 -0400

    Formatting.

--- analyze.py
+++ analyze.py
@@ -23,13 +23,13 @@ today = datetime.datetime.now(LocalTimezone()).strftime('%Y-%m-%d %Z')
 parser = argparse.ArgumentParser(description="Analyze probe results for estimates of peer distribution and network interconnectedness; generate plots.")
 
 # Options.
-parser.add_argument('-d', dest="databaseFile", default="database.sql",\
+parser.add_argument('-d', dest="databaseFile", default="database.sql",
                     help="Path to database file. Default \"database.sql\"")
-parser.add_argument('-T', '--recentHours', dest="recentHours", default=168, type=int,\
+parser.add_argument('-T', '--recentHours', dest="recentHours", default=168, type=int,
                     help="Number of hours for which a probe is considered recent. Used for peer count histogram and link lengths. Default 168 - one week.")
-parser.add_argument('--histogram-max', dest="histogramMax", default=50, type=int,\
+parser.add_argument('--histogram-max', dest="histogramMax", default=50, type=int,
                     help="Maximum number of peers to consider for histogram generation; anything more than that is included in the maximum. Default 50.")
-parser.add_argument('-q', dest='quiet', default=False, action='store_true',\
+parser.add_argument('-q', dest='quiet', default=False, action='store_true',
                     help='Do not print status updates.')
 parser.add_argument('--round-robin', dest='rrd', default='size.rrd',
                     help='Path to round robin network and store size database file.')
