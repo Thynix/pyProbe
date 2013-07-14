83aa91410863ef2704452115fee5a683f0be65ce
Steve Dougherty <steve@asksteved.com>
2013-07-14 17:01:11 -0400

    Whitespace; line lengths.

--- fnprobe/time.py
+++ fnprobe/time.py
@@ -2,14 +2,15 @@ import calendar
 
 
 def toPosix(dt):
-	"""
-	Returns the UTC POSIX timestamp for a datetime.
-	"""
-	return int(calendar.timegm(dt.utctimetuple()))
+    """
+    Returns the UTC POSIX timestamp for a datetime.
+    """
+    return int(calendar.timegm(dt.utctimetuple()))
+
 
 def totalSeconds(delta):
-	# Duration in seconds. Python 2.7 introduced timedelta.total_seconds()
-	# but this should run on Python 2.6. (Version in Debian Squeeze.)
-	# Seconds per day: 24 hours per day * 60 minutes per hour * 60 seconds per minute = 86400
-	# Seconds per microsecond: 1/1000000
-	return delta.days * 86400 + delta.seconds + delta.microseconds / 1000000
+    # Duration in seconds. Python 2.7 introduced timedelta.total_seconds()
+    # but this should run on Python 2.6. (Version in Debian Squeeze.)
+    # Seconds per day: 24 hours per day * 60 minutes per hour * 60 seconds per
+    # minute = 86400; seconds per microsecond: 1/1000000
+    return delta.days * 86400 + delta.seconds + delta.microseconds / 1000000
